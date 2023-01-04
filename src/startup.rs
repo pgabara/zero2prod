f66ac50413714ef16102001bc59cd0d14081ea04
Piotr Gabara <piotr.x.gabara@gmail.com>
2023-01-04 12:29:39 +0100

    Setup tracing

--- src/startup.rs
+++ src/startup.rs
@@ -1,6 +1,7 @@
 use actix_web::{dev::Server, web, App, HttpServer};
 use sqlx::PgPool;
 use std::net::TcpListener;
+use tracing_actix_web::TracingLogger;
 
 use crate::routes;
 
@@ -8,6 +9,7 @@ pub fn run(listener: TcpListener, connection_pool: PgPool) -> std::io::Result<Se
     let connection_pool = web::Data::new(connection_pool);
     let server = HttpServer::new(move || {
         App::new()
+            .wrap(TracingLogger::default())
             .route("/health_check", web::get().to(routes::health_check))
             .route("/subscriptions", web::post().to(routes::subscribe))
             .app_data(connection_pool.clone())
